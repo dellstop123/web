a2fcf9065c9c357abe23f56960d929207f6937dd
Guneet Singh <guneetkhalsa71@gmail.com>
2023-07-25 21:17:24 +0530

    new code added

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+// src/components/About.js
+
+import React from "react"
+import "./about.scss"
+
+const About = () => {
+  return (
+    <section className="about">
+      <h2>About Us</h2>
+      <p>This is the About section where we tell visitors about our company.</p>
+    </section>
+  )
+}
+
+export default About
---
+++ src/components/Hero.js
@@ -0,0 +1,18 @@
+// src/components/Hero.js
+
+import React from "react"
+import background from "../images/icon.png"
+import "./hero.scss"
+
+const Hero = () => {
+  return (
+    <section className="hero" style={{ backgroundImage: `url(${background})` }}>
+      <div className="hero-content">
+        <h1>Welcome to Our Website</h1>
+        <p>This is the Home section of our single-page website.</p>
+      </div>
+    </section>
+  )
+}
+
+export default Hero
