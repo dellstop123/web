8d200f5c7a884ee38060fe06b7865adc1f02aa42
Guneet Singh <guneetkhalsa71@gmail.com>
2023-07-29 18:45:47 +0530

    Language meta added

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -28,7 +28,7 @@ const SEO = ({ title, description, keywords, image }) => (
           <meta name="description" content={seo.description} />
           <meta name="keywords" content={seo.keywords} />
           <meta name="robots" content="index,follow" />
-          <meta name="lan" content="en" />
+          <meta lang="en" />
         </Helmet>
       )
     }}
